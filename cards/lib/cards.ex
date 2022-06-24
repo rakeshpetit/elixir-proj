26f8efc37230939e8ff8301e429a0c72fdf43000
Rakesh Arunachalam <rakesh@thoughtbot.com>
2022-06-24 10:45:06 +0100

    Add deal, save and load functions

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -15,4 +15,24 @@ defmodule Cards do
   def shuffle(deck) do
     Enum.shuffle(deck)
   end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write!(filename, binary)
+  end
+
+  def load(filename) do
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term(binary)
+      {:error, _} -> "That files does not exist"
+    end
+  end
 end
