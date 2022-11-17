eb0fe9ffecfcacc21bcc53f974caee3c9cd5f783
Rakesh Arunachalam <rakesh@thoughtbot.com>
2022-11-17 00:10:00 +0000

    Add topics not working right now

---
+++ discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -0,0 +1,22 @@
+defmodule DiscussWeb.TopicController do
+  use DiscussWeb, :controller
+
+  alias DiscussWeb.Topic
+
+  def index(conn, _params) do
+  end
+
+  def new(conn, _params) do
+    changeset = Topic.changeset(%Topic{}, %{})
+    render(conn, "new.html", changeset: changeset)
+  end
+
+  def create(conn, %{"topic" => topic}) do
+    changeset = Topic.changeset(%Topic{}, topic)
+
+    case Repo.insert(changeset) do
+      {:ok, post} -> IO.inspect(post)
+      {:error, changeset} -> render(conn, "new.html", changeset: changeset)
+    end
+  end
+end
