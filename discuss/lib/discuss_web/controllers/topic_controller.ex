e1a4f5a1a3879a2c071c570ecd1616ea8a12ad75
Rakesh Arunachalam <rakesh@thoughtbot.com>
2022-11-18 14:00:56 +0000

    Edit topic works

--- discuss/lib/discuss_web/controllers/topic_controller.ex
+++ discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -33,4 +33,19 @@ defmodule DiscussWeb.TopicController do
 
     render(conn, "edit.html", changeset: changeset, topic: topic)
   end
+
+  def update(conn, %{"id" => topic_id, "topic" => topic}) do
+    old_topic = Repo.get(Topic, topic_id)
+    changeset = Topic.changeset(old_topic, topic)
+
+    case Repo.update(changeset) do
+      {:ok, _topic} ->
+        conn
+        |> put_flash(:info, "Topic Updated")
+        |> redirect(to: Routes.topic_path(conn, :index))
+
+      {:error, changeset} ->
+        render(conn, "edit.html", changeset: changeset, topic: old_topic)
+    end
+  end
 end
