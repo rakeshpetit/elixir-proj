82b9492db32a66b74faba42eb0abe75ed96c6657
Rakesh Arunachalam <rakesh@thoughtbot.com>
2022-11-27 12:54:46 +0000

    Add check_topic_owner plug
    - Add a check_topic_owner function to check if the user is the owner of
      the topic.
    - Use the topic owner to hide the Edit/Delete options for a topic.

--- discuss/lib/discuss_web/controllers/topic_controller.ex
+++ discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -4,6 +4,7 @@ defmodule DiscussWeb.TopicController do
   alias DiscussWeb.Topic
 
   plug DiscussWeb.Plugs.RequireAuth when action in [:new, :create, :edit, :update, :delete]
+  plug :check_topic_owner when action in [:edit, :update, :delete]
 
   def index(conn, _params) do
     topics = Repo.all(Topic)
@@ -61,5 +62,18 @@ defmodule DiscussWeb.TopicController do
     conn
     |> put_flash(:info, "Topic Deleted")
     |> redirect(to: Routes.topic_path(conn, :index))
+  end
+
+  def check_topic_owner(conn, _params) do
+    %{params: %{"id" => topic_id}} = conn
+
+    if Repo.get(Topic, topic_id).user_id == conn.assigns.user.id do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You are not the owner of this topic")
+      |> redirect(to: Routes.topic_path(conn, :index))
+      |> halt()
+    end
   end
 end
